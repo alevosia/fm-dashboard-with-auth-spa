50949a939423ecaffbf3380a881bb78acba855eb
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2020-09-23 18:55:44 +0800

    Setup Netlify Identity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import { IdentityContextProvider } from 'react-netlify-identity-widget'
 import './layout.css'
 
+const netlifyURL = 'https://fm-dashboard-with-auth-spa.netlify.app/'
+
 const Layout = ({ children }) => (
-	<>
+	<IdentityContextProvider url={netlifyURL}>
 		<header>
 			<Link to="/">JAMStack App</Link>
 		</header>
 		<main>{children}</main>
-	</>
+	</IdentityContextProvider>
 )
 
 export default Layout
